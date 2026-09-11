c1d03edd4d9fef532c9c4d9661970c07b49c8c73
Praveen Kumar Yadav <152773181+Coder-PKyadav@users.noreply.github.com>
2026-09-11 11:48:27 +0530

    Time: 283 ms (43.11%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+select s.machine_id,
+round(avg(e.timestamp-s.timestamp),3) as processing_time
+from
+Activity as s #start side
+join Activity as e# end side
+#we want same machine id and same process id
+on s.machine_id=e.machine_id 
+and s.process_id=e.process_id
+and s.activity_type="start"
+and e.activity_type="end"
+group by machine_id;
+
+
+
+
+
+
+
